eb69ae6b8676c67b9f2db7219b9fa14a815f5508
Yana <ayh916@gmail.com>
2021-09-25 08:51:48 +1000

    added service youtube class

--- src/app.jsx
+++ src/app.jsx
@@ -4,36 +4,18 @@ import SearchHeader from './components/search-header/search-header';
 import { useState, useEffect } from 'react';
 import VideoList from './components/video-list/video-list';
 
-function App() {
+function App({ youtube }) {
   const [videos, setVideos] = useState([])
-
   const search = (query) => {
-
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    }
-
-    fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&type=video&q=${query}&key=AIzaSyDVurpLuofGfRPY-YLa67zIPMSfBiu1aGw`, requestOptions)
-      .then(response => response.json())
-      .then(result => result.items.map(item => ({ ...item, id: item.id.videoId })))
-      .then(items => setVideos(items))
-      .catch(error => console.log('error', error));
-
+    youtube
+      .search(query)
+      .then(videos => setVideos(videos))
   }
-
-
   useEffect(() => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    }
-    fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDVurpLuofGfRPY-YLa67zIPMSfBiu1aGw", requestOptions)
-      .then(response => response.json())
-      .then(result => setVideos(result.items))
-      .catch(error => console.log('error', error));
+    youtube
+      .mostPopular()
+      .then(videos => setVideos(videos))
   }, [])
-
 
   return (
     <div className={styles.app}>
---
+++ src/service/youtube.js
@@ -0,0 +1,30 @@
+class Youtube {
+    constructor(key) {
+        this.key = key
+        this.getRequestOptions = {
+            method: 'GET',
+            redirect: 'follow'
+        }
+    }
+
+    async mostPopular() {
+        const response = await fetch(
+            `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${this.key}`,
+            this.getRequestOptions)
+        const result = await response.json()
+        return result.items
+
+    }
+
+    async search(query) {
+        const response = await fetch(
+            `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&type=video&q=${query}&key=${this.key}`, this.getRequestOptions)
+        const result = await response.json()
+        const items = result.items.map(item => ({ ...item, id: item.id.videoId }))
+        return items
+
+    }
+
+}
+
+export default Youtube
