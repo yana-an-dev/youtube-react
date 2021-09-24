db829430a8be2c3a064100384decec26d21c80e7
Yana <ayh916@gmail.com>
2021-09-24 11:50:09 +1000

    SearchHeader

--- src/app.jsx
+++ src/app.jsx
@@ -1,30 +1,45 @@
 
-import './app.css'
-import HeaderContainer from './containers/header';
+import styles from './app.module.css'
+import SearchHeader from './components/search-header/search-header';
 import { useState, useEffect } from 'react';
 import VideoList from './components/video-list/video-list';
 
 function App() {
   const [videos, setVideos] = useState([])
 
+  const search = (query) => {
+
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    }
+
+    fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&key=AIzaSyDVurpLuofGfRPY-YLa67zIPMSfBiu1aGw`, requestOptions)
+      .then(response => response.json())
+      .then(result => result.items.map(item => ({ ...item, id: item.id.videoId })))
+      .then(items => setVideos(items))
+      .catch(error => console.log('error', error));
+
+  }
+
+
   useEffect(() => {
     const requestOptions = {
       method: 'GET',
       redirect: 'follow'
-    };
+    }
     fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDVurpLuofGfRPY-YLa67zIPMSfBiu1aGw", requestOptions)
       .then(response => response.json())
       .then(result => setVideos(result.items))
       .catch(error => console.log('error', error));
-
-
   }, [])
 
+
   return (
-    <>
-      <HeaderContainer />
+    <div className={styles.app}>
+      <SearchHeader onSearch={search} />
       <VideoList videos={videos} />
-    </>
+    </div>
   );
 }
 
---
+++ src/components/search-header/search-header.jsx
@@ -0,0 +1,64 @@
+import React, { useRef } from 'react'
+import styles from './search-header.module.css'
+
+const SearchHeader = ({ onSearch }) => {
+    const inputRef = useRef()
+
+    const handleSearch = () => {
+        const value = inputRef.current.value
+        onSearch(value)
+    }
+    const onClick = () => {
+        handleSearch()
+    }
+
+    const onKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            handleSearch()
+        }
+    }
+
+    return (
+        <header className={styles.header}>
+            <div className={styles.logo}>
+                <img className={styles.img} src="images/logo.png" alt="logo" />
+                <h1 className={styles.title}>YanaTube</h1>
+
+            </div>
+
+            <input
+                ref={inputRef}
+                className={styles.input}
+                type="search"
+                placeholder="search..."
+                onKeyDown={onKeyDown}
+
+            />
+            <button
+                type="submit"
+                className={styles.button}
+                onClick={onClick}>
+                <i className="fas fa-search"></i>
+            </button>
+
+        </header>
+    );
+}
+export default SearchHeader;
+
+
+
+// export default function Header({ children, ...restProps }) {
+//     return <Frame {...restProps}>{children}</Frame>
+// }
+
+// Header.Logo = ({ children }) => {
+//     return <Logo>{children}</Logo>
+// }
+
+// Header.Search = ({ children, ...restProps }) => {
+//     return <Search placeholder="Search.." {...restProps}>{children}</Search>
+// }
+// Header.SearchButton = ({ children, ...restProps }) => {
+//     return <SearchButton {...restProps}>{children}</SearchButton>
+// }
